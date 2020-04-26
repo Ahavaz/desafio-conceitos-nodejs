af6c7236802982fed8966c5ac2fd2279821cd7a2
Ahavaz <lucas.cesar.8@hotmail.com>
2020-04-26 17:02:08 -0300

    finish challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repo);
+
+  return response.send(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).send({ message: "Repositório inexistente" });
+  }
+
+  repositories[repoIndex] = { ...repositories[repoIndex], title, url, techs };
+
+  return response.send(repositories[repoIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).send({ message: "Repositório inexistente" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.sendStatus(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).send({ message: "Repositório inexistente" });
+  }
+
+  const likes = ++repositories[repoIndex].likes;
+
+  repositories[repoIndex] = {
+    ...repositories[repoIndex],
+    likes,
+  };
+
+  return response.send(repositories[repoIndex]);
 });
 
 module.exports = app;
